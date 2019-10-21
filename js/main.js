7abb703e219609d5ebae6b4a475281330d417de9
alexk08 <akapacuk@gmail.com>
2019-10-21 12:51:43 +0400

    заканчивает обработку событий по эффектам фото в рамках данного дз, остается валидация хэштегов

--- js/main.js
+++ js/main.js
@@ -113,9 +113,11 @@ picturesListElement.appendChild(createFragment(photos));
 
 // При наступлении события chahge на элементе с id #upload-file показываем форму редактирования изображения, т.е. у блока с классом img-upload__overlay удаляем класс .hidden
 
-//Далее пишем обработчик на закрытие формы (вышеуказанному блоку добавляем класс hidden по клику на крестик мышкой либо клавишей Enter при фокусе на крестике, либо по нажатию на клавишу Esc), при это сбрасываем значение поля выбора файла (блок с id #upload-file)
+// Далее пишем обработчик на закрытие формы (вышеуказанному блоку добавляем класс hidden по клику на крестик мышкой либо клавишей Enter при фокусе на крестике, либо по нажатию на клавишу Esc), при это сбрасываем значение поля выбора файла (блок с id #upload-file)
 
-
+/**
+ * Открытие закрытие формы настройки фото
+ */
 var fileUploadControl = document.querySelector('#upload-file');
 var editImageControl = document.querySelector('.img-upload__overlay');
 var closeButton = editImageControl.querySelector('.img-upload__cancel');
@@ -132,35 +134,37 @@ var escPressHandler = function (evt) {
 var openPopup = function () {
   editImageControl.classList.remove('hidden');
   document.addEventListener('keydown', escPressHandler);
+  effectLevelField.classList.add('hidden');
 };
 
 var closePopup = function () {
   editImageControl.classList.add('hidden');
   document.removeEventListener('keydown', escPressHandler);
-  fileUploadControl.value = "";
+  fileUploadControl.value = '';
 };
 
-fileUploadControl.addEventListener('change', function (evt) {
+fileUploadControl.addEventListener('change', function () {
   // evt.preventDefault();
   openPopup();
 });
 
-closeButton.addEventListener('click', function (evt) {
+closeButton.addEventListener('click', function () {
   // evt.preventDefault();
   closePopup();
 });
 
-//При наступлении события "click" на кнопке c классом .scale__control--smaller должно изменяться значение поля input с классом .scale__control--value на длину шага, если текущее его значение больше минимального.
+// При наступлении события "click" на кнопке c классом .scale__control--smaller должно изменяться значение поля input с классом .scale__control--value на длину шага, если текущее его значение больше минимального.
 
-//При наступлении события "click" на кнопке c классом .scale__control--bigger должно изменяться значение поля input с классом .scale__control--value на длину шага, если текущее его значение меньше максимального значения
+// При наступлении события "click" на кнопке c классом .scale__control--bigger должно изменяться значение поля input с классом .scale__control--value на длину шага, если текущее его значение меньше максимального значения
 
-//Минимальное, максимальное значения, длину шага и значение .scale__control--value по умолчанию задаем через постоянные переменные.
+// Минимальное, максимальное значения, длину шага и значение .scale__control--value по умолчанию задаем через постоянные переменные.
 
 var scaleSmaller = document.querySelector('.scale__control--smaller');
 var scaleBigger = document.querySelector('.scale__control--bigger');
 var scaleControl = document.querySelector('.scale__control--value');
 var scaleValue = Number(scaleControl.value.slice(0, -1));
 var image = document.querySelector('.img-upload__preview').firstElementChild;
+var effectLevelField = document.querySelector('.effect-level');
 
 var Scale = {
   STEP: 25,
@@ -169,74 +173,85 @@ var Scale = {
 };
 
 /**
- *
+ * Функция расчета увеличения масштаба фото
  */
 var decreaseScale = function () {
   if (scaleValue > Scale.MIN) {
-    scaleValue = scaleValue - Scale.STEP;
-    scaleControl.value = scaleValue + "%";
-    image.style.transform = "scale(" + scaleValue * 0.01 + ")";
+    scaleValue -= Scale.STEP;
+    scaleControl.value = scaleValue + '%';
+    image.style.transform = 'scale(' + scaleValue * 0.01 + ')';
   }
 };
 
 /**
- *
+ * Функция расчета уменьшения масштаба фото
  */
 var increaseScale = function () {
   if (scaleValue < Scale.MAX) {
-    scaleValue = scaleValue + Scale.STEP;
-    scaleControl.value = scaleValue + "%";
-    image.style.transform = "scale(" + scaleValue * 0.01 + ")";
+    scaleValue += Scale.STEP;
+    scaleControl.value = scaleValue + '%';
+    image.style.transform = 'scale(' + scaleValue * 0.01 + ')';
   }
 };
 
-scaleSmaller.addEventListener('click', function (evt) {
+scaleSmaller.addEventListener('click', function () {
   // evt.preventDefault();
   decreaseScale();
 });
 
-scaleBigger.addEventListener('click', function (evt) {
+scaleBigger.addEventListener('click', function () {
   // evt.preventDefault();
   increaseScale();
 });
 
-scaleControl.addEventListener('change', function () {
-  console.log(1);
-});
+// scaleControl.addEventListener('change', function () {
+//   console.log(1);
+// });
 
 // var scaleValue = Number(scaleControl.getAttribute("value").slice(0, -1));
 // scaleControl.setAttribute("value", (String(scaleValue) + "%"));
 // scaleControl.setAttribute("value", (String(scaleValue) + "%"));
 
-//При возникновении события change на scaleControl блок с изображением внутри блока с классом .img-upload__preview стилизуется свойством transform: scale(X). Где X значение scaleValue умноженное на 0,01.
+// При возникновении события change на scaleControl блок с изображением внутри блока с классом .img-upload__preview стилизуется свойством transform: scale(X). Где X значение scaleValue умноженное на 0,01.
 
-//При закрытии формы настройки фотографии, значение свойства transform у фото не сбрасывается. И при загрузке новой фотографии, масштаб остается не по-умолчанию. Нужно подумать как устанавливать значение по-умолчанию при закрытии окна.
+// При закрытии формы настройки фотографии, значение свойства transform у фото не сбрасывается. И при загрузке новой фотографии, масштаб остается не по-умолчанию. Нужно подумать как устанавливать значение по-умолчанию при закрытии окна.
 
-//Не получается обрабать событие изменения значения (событие change не отлавливается) input'а scaleControl. И в разметке изменение value не отображается.
+// Не получается обрабать событие изменения значения (событие change не отлавливается) input'а scaleControl. И в разметке изменение value не отображается.
 
 var effects = document.querySelector('.effects');
 
 /**
- * Функция обработчик события клика по input [type=radio] с делегированием на элемент fieldset. Результатом функцииявляется присвоение класса фотографии.
+ * Функция обработчик события клика по input [type=radio] с делегированием на элемент fieldset. Происходит выбор эффекта для фотографии и сброс уровня нассыщенности до значения по умолчанию.
  * Фунция обработчик события. Определяет эффект, накладываемый на изображение.
  */
+
 var effectClickHandler = function (evt) {
-  if (evt.target.matches(".effects__radio")) {
+  if (evt.target.matches('.effects__radio')) {
     var effectName = evt.target.value;
+    effectLevelField.classList.remove('hidden');
+    effectLevel.setAttribute('value', 100);
+    image.style.filter = null;
     if (image.classList.length === 0) {
       image.classList.add('effects__preview--' + effectName);
     } else {
-      image.className = "";
+      image.className = '';
       image.classList.add('effects__preview--' + effectName);
+    }
+    if (image.className === 'effects__preview--none') {
+      image.removeAttribute('style');
+      effectLevelField.classList.add('hidden');
     }
   }
 };
 
 effects.addEventListener('click', effectClickHandler);
 
-//хорошая ли это практика заводить переменную внитри условия? или можно просто в дальнейшем оперировать значением атрибута evt.target.value, без записи в переменную
+// хорошая ли это практика заводить переменную внитри условия? или можно просто в дальнейшем оперировать значением атрибута evt.target.value, без записи в переменную
 // evt.preventDefault(); //где лучше писать отмену действия по умолчанию? при вызове метода addEventListner или в описании функции. короч вообще лучше почитать про отмену действия по умолчанию. здесь не работает потому что отменяется распространение? stoppropagation?
 
+/**
+ * Обработчик события reset: удаление класса с фото и удаление стилей фильтра.
+ */
 var uploadImageForm = document.querySelector('.img-upload__form');
 
 uploadImageForm.addEventListener('reset', function () {
@@ -244,47 +259,74 @@ uploadImageForm.addEventListener('reset', function () {
   image.removeAttribute('class');
 });
 
-/**
- * функция рассчета уровня нассыщенности
- */
-
-var levelPin = document.querySelector('.effect-level__pin');
-var levelLine = document.querySelector('.effect-level__line');
-var effectLevel = document.querySelector('.effect-level__value')
-
-
-//Для эффекта «Хром» — filter: grayscale(0..1);
+// Для эффекта «Хром» — filter: grayscale(0..1);
 // Для эффекта «Сепия» — filter: sepia(0..1);
 // Для эффекта «Марвин» — filter: invert(0..100%);
 // Для эффекта «Фобос» — filter: blur(0..3px);
 // Для эффекта «Зной» — filter: brightness(1..3);
 // Для эффекта «Оригинал» CSS-стили filter удаляются.
 
-var pinMouseUpHandler = function(evt) {
+
+var levelPin = document.querySelector('.effect-level__pin');
+var levelLine = document.querySelector('.effect-level__line');
+var effectLevel = document.querySelector('.effect-level__value');
+
+var filters = [
+  {
+    effectName: 'chrome',
+    filterName: 'grayscale',
+    minValue: 0,
+    maxValue: 1,
+    measurementUnit: ''
+  },
+  {
+    effectName: 'sepia',
+    filterName: 'sepia',
+    minValue: 0,
+    maxValue: 1,
+    measurementUnit: ''
+  },
+  {
+    effectName: 'marvin',
+    filterName: 'invert',
+    minValue: 0,
+    maxValue: 100,
+    measurementUnit: '%'
+  },
+  {
+    effectName: 'phobos',
+    filterName: 'blur',
+    minValue: 0,
+    maxValue: 3,
+    measurementUnit: 'px'
+  },
+  {
+    effectName: 'heat',
+    filterName: 'brightness',
+    minValue: 1,
+    maxValue: 3,
+    measurementUnit: ''
+  }
+  // {
+  //   effectName: "none",
+  //   filterName: "",
+  //   minValue: 0,
+  //   maxValue: 0,
+  //   measurementUnit: ""
+  // }
+];
+
+/**
+ * функция обработчик события отпускание клавиши мышки: рассчет уровня нассыщенности по положению ползунка
+ */
+
+var pinMouseUpHandler = function (evt) {
   var coefficient = (evt.clientX - levelLine.getBoundingClientRect().left) / levelLine.getBoundingClientRect().width;
-  effectLevel.setAttribute("value", coefficient * 100);
-  if (image.className === "effects__preview--chrome") {
-    image.style.filter = "grayscale(" + coefficient + ")";
-  }
-
-  if (image.className === "effects__preview--sepia") {
-    image.style.filter = "sepia(" + coefficient + ")";
-  }
-
-  if (image.className === "effects__preview--marvin") {
-    image.style.filter = "invert(" + (coefficient * 100)+ "%)";
-  }
-
-  if (image.className === "effects__preview--phobos") {
-    image.style.filter = "blur(" + (coefficient * (3 - 0) + 0) + "px)";
-  }
-
-  if (image.className === "effects__preview--heat") {
-    image.style.filter = "brightness(" + (coefficient * (3 - 1) + 1) + ")";
-  }
-
-  if (image.className === "effects__preview--none") {
-    image.style.filter = " ";
+  effectLevel.setAttribute('value', coefficient * 100);
+  for (var i = 0; i < filters.length; i++) {
+    if (image.className === ('effects__preview--' + filters[i].effectName)) {
+      image.style.filter = filters[i].filterName + '(' + (coefficient * (filters[i].maxValue - filters[i].minValue) + filters[i].minValue) + filters[i].measurementUnit + ')';
+    }
   }
 };
 
